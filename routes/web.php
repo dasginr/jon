d10842ed4f0af355b60a412d60c6d8cc523b3972
Ramazan Daşgın <dasginr@gmail.com>
2024-09-19 09:42:34 +0300

    view metod route olarak güncellendi.

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('posts.index');
-})->name('home');
+Route::view('/', 'posts.index')->name('home');
 
-Route::get('/register', function(){
-    return view('auth.register');
-})->name('register');
+Route::view('/register', 'auth.register')->name('register');
